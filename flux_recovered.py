38480c027d14285f7fbfce56d568ee403ba93fc2
e-koch <koch.eric.w@gmail.com>
2015-08-11 16:37:13 -0600

    Beginnings of general class for handling high and low res data

---
+++ flux_recovered.py
@@ -0,0 +1,116 @@
+
+'''
+Find the amount of emission recovered in a high-resolution map, based on a low-resolution map
+'''
+
+from spectral_cube import SpectralCube
+import numpy as np
+from radio_beam import Beam
+from astropy.convolution import convolve
+from astropy import units as u
+from astropy import wcs
+
+
+class MultiResObs(object):
+    """
+    Object to hold observations of the same object at different resolutions.
+    This is intended for matching and analyzing high-res (interferometric)
+    and low-res (single dish) observations.
+    """
+    def __init__(self, highres, lowres):
+        super(MultiResObs, self).__init__()
+        self.highres = SpectralCube.read(highres)
+        self.lowres = SpectralCube.read(lowres)
+
+        self.lowbeam = Beam.from_fits_header(self.lowres.header)
+        self.highbeam = Beam.from_fits_header(self.highres.header)
+
+        self.combined_beam = self.lowbeam.convolve(self.highbeam)
+
+    def match_coords(self):
+        '''
+        Match the spatial and spectral coordinates of the cubes.
+        '''
+
+        # Are either of the cubes in the correct frame?
+        if self.highres.header['CTYPE1'] != self.lowres.header['CTYPE1']:
+            raise TypeError("ctypes do not match. Are observations in the "
+                            "same projection? Highres: " +
+                            self.highres.header['CTYPE1'] +
+                            " Lowres: " + self.lowres.header['CTYPE1'])
+        elif self.highres.header['CTYPE2'] != self.lowres.header['CTYPE2']:
+            raise TypeError("ctypes do not match. Are observations in the "
+                            "same projection? Highres: " +
+                            self.highres.header['CTYPE2'] +
+                            " Lowres: " + self.lowres.header['CTYPE2'])
+
+        # Determine which cube should be slice down so both have the same
+        # spatial coverage.
+
+        all_extrema = []
+
+        long_slice = self.lowres.longitude_extrema > self.highres.longitude_extrema
+
+        if long_slice[0]:
+            all_extrema.append(self.highres.longitude_extrema[0])
+        else:
+            all_extrema.append(self.lowres.longitude_extrema[0])
+
+        if long_slice[1]:
+            all_extrema.append(self.highres.longitude_extrema[1])
+        else:
+            all_extrema.append(self.lowres.longitude_extrema[1])
+
+        lat_slice = self.lowres.latitude_extrema > self.highres.latitude_extrema
+
+        if lat_slice[0]:
+            all_extrema.append(self.highres.latitude_extrema[1])
+        else:
+            all_extrema.append(self.lowres.latitude_extrema[1])
+
+        if lat_slice[1]:
+            all_extrema.append(self.highres.latitude_extrema[0])
+        else:
+            all_extrema.append(self.lowres.latitude_extrema[0])
+
+        # Apply common slicing to both
+
+        self.highres = \
+            self.highres.subcube(xlo=all_extrema[0], xhi=all_extrema[1],
+                                 ylo=all_extrema[2], yhi=all_extrema[3])
+
+        self.lowres = \
+            self.lowres.subcube(xlo=all_extrema[0], xhi=all_extrema[1],
+                                ylo=all_extrema[2], yhi=all_extrema[3])
+
+        # Now match the spectral extends
+
+        low_spec = \
+            self.highres.spectral_extrema[0] if \
+            self.highres.spectral_extrema[0] > self.lowres.spectral_extrema[0]\
+            else self.lowres.spectral_extrema[0]
+
+        high_spec = \
+            self.highres.spectral_extrema[1] if \
+            self.highres.spectral_extrema[1] < self.lowres.spectral_extrema[1]\
+            else self.lowres.spectral_extrema[1]
+
+        self.highres = self.highres.spectral_slab(low_spec, high_spec)
+        self.lowres = self.lowres.spectral_slab(low_spec, high_spec)
+
+        return self
+
+    def convert_to(self, unit=u.K):
+        '''
+        Convert both sets to common brightness units.
+        '''
+        pass
+
+    def convolve_to_low(self):
+        '''
+        Convolve highres to resolution of lowres.
+        '''
+        pass
+
+    def flux_recovered(self):
+        pass
