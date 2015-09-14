71f12ee9d98600879fe2699c4b1f6496334aa9cc
e-koch <koch.eric.w@gmail.com>
2015-09-13 19:04:12 -0600

    Add plotting

--- making_figures/hi_co_absorp_spectra.py
+++ making_figures/hi_co_absorp_spectra.py
@@ -24,7 +24,7 @@ hi_cube = hi_cube.subcube(xlo=long_extrema[1],
 # Now find the position in each cube corresponding to a given
 # coordinate
 
-posn = SkyCoord("1h33m25.684", "+30d32m17.009", frame='icrs')
+posn = SkyCoord("1h33m14.905", "+30d33m17.479", frame='icrs')
 
 
 def get_closest_posn(posn, spatial_footprint):
@@ -49,6 +49,28 @@ hi_spectrum = hi_cube[:, hi_posn[0], hi_posn[1]]
 
 co21_spectrum = co21_cube[:, co21_posn[0], co21_posn[1]]
 
+ax = p.subplot(131)
+ax.plot(hi_spectrum.spectral_axis/1000., hi_spectrum.value)
+ax.set_xlabel('Velocity (km/s)')
+ax.set_ylabel('HI Surface Brightness (K)')
+for tick in ax.get_xticklabels():
+    tick.set_rotation(45)
+ax_2 = ax.twinx()
+ax_2.plot(co21_spectrum.spectral_axis/1000., co21_spectrum.value, 'g')
+ax_2.set_ylabel('CO(2-1) Surface Brightness (K)')
 
-p.plot(hi_spectrum.spectral_axis, hi_spectrum.value)
-p.plot(co21_spectrum.spectral_axis, co21_spectrum.value)
+p.subplot(233)
+hi_mom0 = np.nansum(hi_cube.filled_data[:].value, axis=0)
+hi_slice = [slice(hi_posn[0]-30, hi_posn[0]+30),
+            slice(hi_posn[1]-30, hi_posn[1]+30)]
+p.imshow(hi_mom0[hi_slice], origin='lower')
+p.scatter([30], [30], c='r', s=40)
+
+p.subplot(236)
+co21_mom0 = co21_cube.moment0().value
+co21_slice = [slice(co21_posn[0]-30, co21_posn[0]+30),
+              slice(co21_posn[1]-30, co21_posn[1]+30)]
+p.imshow(co21_mom0[co21_slice], origin='lower')
+p.scatter([30], [30], c='r', s=40)
+
+p.savefig("hi_co_overlay_"+new_coord.to_string('hmsdms').replace(' ', '_')+".pdf")
