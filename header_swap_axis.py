8162c7e8994e083bde0a3bb1519fbe7f91468170
e-koch <koch.eric.w@gmail.com>
2015-10-16 15:11:45 -0600

    Generalized header swapaxes function

---
+++ header_swap_axis.py
@@ -0,0 +1,27 @@
+
+'''
+Swap the axes in a header, without losing keys to WCS
+'''
+
+from astropy.wcs import WCS
+
+
+def header_swapaxes(header, ax1, ax2):
+    '''
+    '''
+
+    mywcs = WCS(header)
+
+    new_hdr = mywcs.swapaxes(ax1, ax2).to_header()
+
+    lost_keys = list(set(header.keys()) - set(new_hdr.keys()))
+
+    for key in lost_keys:
+        if str(ax1+1) in key:
+            new_hdr[key.replace(str(ax1+1), str(ax2+1))] = header[key]
+        elif str(ax2+1) in key:
+            new_hdr[key.replace(str(ax2+1), str(ax1+1))] = header[key]
+        else:
+            new_hdr[key] = header[key]
+
+    return new_hdr
